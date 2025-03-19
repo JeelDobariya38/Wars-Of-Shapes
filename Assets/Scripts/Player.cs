cc723bb3d62171e32b46a4b2a6b0b3a0f8ed7403
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-03-19 22:00:16 +0530

    add a health ui

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,24 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     public int maxHealth;
+    public Text healthText;
 
     private int health;
 
     void Start() {
         health = maxHealth;
+        updateHealthText();
     }
 
     public void takeDamage(int damage) {
         health -= damage;
-
+        updateHealthText();
         if (health <= 0) {
             Debug.Log("Player Died!!");
             SceneManager.LoadScene(0);
         }
+    }
+
+    void updateHealthText() {
+        healthText.text = "Health: " + health.ToString();
     }
 }
