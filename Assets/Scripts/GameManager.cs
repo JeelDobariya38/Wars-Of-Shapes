24ed3bdb878b385a95f2e5e006acf7e2f4f34790
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-03-26 17:25:28 +0530

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject pausemenu;
+    public GameObject mainmenu;
+
+
     public void GameStart() {
         SceneManager.LoadScene(1);
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        mainmenu.SetActive(false);
+        pausemenu.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        mainmenu.SetActive(true);
+        pausemenu.SetActive(false);
     }
 
     public void Quit()
