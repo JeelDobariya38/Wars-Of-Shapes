d89d7f59031fec38b10b5af4ff8b9082abd026ed
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-03-30 20:58:48 +0530

    make the enemy collision work

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,11 +12,13 @@ public class Enemy : MonoBehaviour
 
     private Transform target;
     private float timeBtwShootCount;
+    private Rigidbody2D rb;
     
     // Start is called before the first frame update
     void Start()
     {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
         timeBtwShootCount = 0;
     }
 
@@ -30,14 +32,21 @@ public class Enemy : MonoBehaviour
         } else {
             timeBtwShootCount += Time.deltaTime;
         }
+    }
 
-        // Movement
-        if (Vector2.Distance(transform.position, target.position) < retreatDistance) {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, -speed * Time.deltaTime);
+    void FixedUpdate()
+    {
+        Vector2 direction = Vector2.zero;
+        float distance = Vector2.Distance(transform.position, target.position);
+
+        if (distance < retreatDistance) {
+            direction = (transform.position - target.position).normalized;
         }
-        else if (Vector2.Distance(transform.position, target.position) > stoppingDistance) {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        else if (distance > stoppingDistance) {
+            direction = (target.position - transform.position).normalized;
         }
+
+        rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
     }
 
     void Shoot() {
