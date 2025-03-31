9c1f40ab1f3ff03b97dd32f42baaeb33f2cba090
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-03-31 23:02:54 +0530

    Adopt to new input system

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,25 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Terresquall;
 
 public class Movement : MonoBehaviour
 {
     public float speed;
+    public PlayerControls controls;
 
     private Rigidbody2D rb;
     private Vector2 moveVelocity;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    private void Awake() {
         rb = GetComponent<Rigidbody2D>();
+        controls = new PlayerControls();
+        controls.Enable();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable() {
+        controls.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 movementInput = new Vector2(VirtualJoystick.GetAxis("Horizontal"), VirtualJoystick.GetAxis("Vertical"));
+        Vector2 movementInput = controls.Player.Move.ReadValue<Vector2>();
         moveVelocity = movementInput.normalized * speed;
     }
 
