0051ae5567888501ed3c388d79433cf8957faaa6
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-09-25 12:57:35 +0530

    lint: format script to use spaces instead of tabs

--- Scripts/hello.gd
+++ Scripts/hello.gd
@@ -1,5 +1,5 @@
 extends Node
 
 func _ready() -> void:
-	print("Hello Shapes!!")
-	print("Let have some war, So that I can get entertain!!")
+    print("Hello Shapes!!")
+    print("Let have some war, So that I can get entertain!!")
